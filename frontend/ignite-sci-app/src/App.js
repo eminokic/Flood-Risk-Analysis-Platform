d0f493040cf9f766b985a18e9b597267a946604d
Emin Okic <emin.okic@mavenwave.com>
2021-07-08 07:37:07 -0500

    Implementing loadError and isLoaded function.

--- frontend/ignite-sci-app/src/App.js
+++ frontend/ignite-sci-app/src/App.js
@@ -15,9 +15,23 @@ export default function App() {
      * You can implement the places api key as follows to avoid redundant rerendering.
      * const libraries = ["places"];
      */
-  const {} = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: ProcessingInstruction.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
+
+  /**
+   * The loadError function is a flag for the case that the map does not render properly. 
+   */
+  if(loadError) {
+    return "Error in rendering map. Try Again!";
+  }
+
+  /**
+   * The isLoaded function checks to see if the map has been rendered each time and properly flags the case if not.
+   */
+  if(!isLoaded) {
+    return "Rendering Map...";
+  }
 
   return <div>I'm the map, i'm the map!</div>
 }
