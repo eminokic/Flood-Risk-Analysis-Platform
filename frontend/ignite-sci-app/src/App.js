8922aa7b90afb154d1cc58b400d1bc1431cc6515
Emin Okic <emin.okic@mavenwave.com>
2021-07-08 07:24:25 -0500

    Refactoring import and default function in app js.

--- frontend/ignite-sci-app/src/App.js
+++ frontend/ignite-sci-app/src/App.js
@@ -7,30 +7,13 @@ import {
 } from "@react-google-maps/api";
 import { formatRelative } from "date-fns";
 import "@reach/combobox/styles.css";
-
-// Media Related Imports
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
 
-export default App;
+  const {} = useLoadScript({
+    googleMapsApiKey: ProcessingInstruction.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  })
+
+  return <div>I'm the map, i'm the map!</div>
+}
