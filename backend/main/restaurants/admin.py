56c9432de1662e66aa09299503115a08f83419b0
Emin Okic <emin.okic@mavenwave.com>
2021-07-12 11:48:59 -0500

    Restructuring database and implementing restaurant model.

--- backend/main/restaurants/admin.py
+++ backend/main/restaurants/admin.py
@@ -1,3 +1,5 @@
+from backend.main.restaurants.models import Restaurant
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Restaurant)
--- backend/main/restaurants/models.py
+++ backend/main/restaurants/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    building_id = models.IntegerField()
+    address = models.CharField(max_length=200)
+    postal_code = models.IntegerField(default=0)
+    risk_value = models.IntegerField(default=0)
+
+    # Setting Up Representation of Restaurant Object
+    def __str__(self):
+        return self.building_id
