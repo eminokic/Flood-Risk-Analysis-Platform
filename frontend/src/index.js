44d653e8a3f887197b762f754e6e450a493d9c82
Emin Okic <emin.okic@mavenwave.com>
2021-08-02 14:24:19 -0500

    checkpoint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../css/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './css/index.css';
+import App from './main/App';
+import reportWebVitals from './javascript/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
