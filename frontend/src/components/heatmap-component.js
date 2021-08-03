8ecefcd6e5d9d51adc4c9605f47a07011d91af00
Emin Okic <emin.okic@mavenwave.com>
2021-08-03 12:45:25 -0500

    Refactoring

--- frontend/src/components/heatmap-component.js
+++ frontend/src/components/heatmap-component.js
@@ -1,9 +1,8 @@
 import React from "react";
-import {GoogleMap} from "@react-google-maps/api";
 import "@reach/combobox/styles.css";
 import Polygon from 'react-polygon'
 
-export default class Okic extends React.Component {
+export default class Heatmap extends React.Component {
   polygonCenter (point) {
     return (
       <circle cx={point[0]} cy={point[1]} r={5} />
