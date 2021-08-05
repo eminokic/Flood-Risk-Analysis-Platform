264592c222fd75b33e93a99b8904d08dcc83d3be
Emin Okic <emin.okic@mavenwave.com>
2021-08-05 10:15:11 -0500

    Refactoring

--- frontend/src/main/App.js
+++ frontend/src/main/App.js
@@ -9,12 +9,8 @@ import {
 import "@reach/combobox/styles.css";
 import '../css/App.css';
 
-import * as locationdata from "../data/LocationData.json"
-
 import Heatmap from "../components/heatmap-component.js"
-import Okic from "../components/okic-component.js"
 import Markers from "../components/markers-component.js"
-import Derek from "../components/derek-component.js"
 import {DataTable} from "../components/data-table-component";
 import logo from '../components/images/Salient-Insure.jpg';
 
@@ -76,7 +72,6 @@ export default function App() {
     
     <Markers></Markers>
     <DataTable></DataTable>
-    <Okic></Okic>
-    <Derek></Derek>
+    <Heatmap></Heatmap>
     </div>
 }
