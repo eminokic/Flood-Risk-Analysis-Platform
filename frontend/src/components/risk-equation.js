30c4376dd4355d975b728410833177d532b37ae8
Emin Okic <emin.okic@mavenwave.com>
2021-08-09 10:45:11 -0500

    Adding risk equation component skeleton

---
+++ frontend/src/components/risk-equation.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import "@reach/combobox/styles.css";
+
+export default class RiskEquation extends Component {
+  render() {
+      //TODO
+      return <div></div>
+    }
+}
