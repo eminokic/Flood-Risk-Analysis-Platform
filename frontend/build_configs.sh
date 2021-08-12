c98812ff6505f17f44623ea002ff822ae29076ad
Walker Obrien <wnobrien@umich.edu>
2021-08-12 10:22:25 -0500

    using https for frontend to backend requests

--- frontend/build_configs.sh
+++ frontend/build_configs.sh
@@ -2,7 +2,7 @@
 
 CONTENT="{
     proxy_set_header Host $DOMAIN;
-    proxy_pass http://$LIFECYCLE-api-svc.$LIFECYCLE.svc;
+    proxy_pass http://api-svc.$LIFECYCLE.svc;
 }"
 
 echo "location /api/ $CONTENT
@@ -10,6 +10,6 @@ location /admin/ $CONTENT
 location /redirect/ $CONTENT
 location /accounts/ $CONTENT" > /etc/nginx/conf.d/api.conf;
 
-echo "window.API_URL = 'http://$DOMAIN/';
+echo "window.API_URL = 'https://$DOMAIN/';
 window.STATIC_BUCKET = '$STATIC_BUCKET';
 window.LIFECYCLE = '$LIFECYCLE';" > /var/www/html/env.js
