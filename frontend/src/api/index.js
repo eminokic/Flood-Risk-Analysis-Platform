40ae5cc606a6e401fd3b966ac0014aaf1851afd3
Walker Obrien <wnobrien@umich.edu>
2021-08-12 10:43:47 -0500

    youre welcome :)

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = window.API_URL;
+const API_URL = window.API_URL || 'http://localhost:8000/';
 
 export const api = () => {
   axios.defaults.withCredentials = true;
