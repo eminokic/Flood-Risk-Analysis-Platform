fc737beda4f228c60c70c1dee00d86a45d2f5418
Emin Okic <emin.okic@mavenwave.com>
2021-08-13 09:57:12 -0500

    Refactoring and adding axios test

--- frontend/src/test/axios-component.test.js
+++ frontend/src/test/axios-component.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import axios from 'axios';
+import { fetchData, API } from '../components/axios-tools/axios-fetching.js';
+
 import HelloDjango from "../components/axios-tools/HelloDjango.js"
 
 test('renders the hello django component.', () => {
   render(<HelloDjango />);
-  //const linkElement = screen.getByText(/learn react/i);
-  //expect(linkElement).toBeInTheDocument();
+});
+
+test('tests api connection.', () => {
+  
+})
+jest.mock('axios');
+ 
+describe('fetchData', () => {
+  it('fetches successfully data from an API', async () => {
+    const data = {};
+ 
+    axios.get.mockImplementationOnce(() => Promise.resolve(data));
+    await expect(fetchData('react')).resolves.toEqual(data);
+ 
+    expect(axios.get).toHaveBeenCalledWith(
+      `${API}/search?query=react`,
+    );
+  });
 });
