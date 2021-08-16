961e1b28a45ddd595bb66e85a11b08d31640adb3
eminokic <emin.okic@mavenwave.com>
2021-08-16 11:17:25 -0500

    Modularizing app.js to a main component

--- frontend/src/main/App.js
+++ frontend/src/main/App.js
@@ -6,10 +6,7 @@ import {
 import "@reach/combobox/styles.css";
 import '../css/App.css';
 
-
-import HomePage from '../components/header-component/header.js';
-import SecondPage from '../components/map-component/map-section.js';
-import ThirdPage from '../components/data-table/analytic-section.js';
+import MainComponent from "../components/main-page/main-component";
 
 export default function App() {
 
@@ -37,20 +34,6 @@ export default function App() {
   if(!isLoaded) {return "Rendering Map...";}
 
   return <div>
-    <head>
-      <title>Salient Insurance</title>
-      <link rel="shortcut icon" type="image/png" href= "./components/images/icon.png" />
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <script type="text/javascript" src="../components/jquery.js"></script>
-		  <script type="text/javascript" src="../components/declarativeToggle.js"></script>
-    </head>
-    <body>
-    <HomePage></HomePage>
-    <div class="background background1"></div>
-    <SecondPage></SecondPage>
-    <div class="background background1"></div>
-    <ThirdPage></ThirdPage>
-    </body>
+    <MainComponent></MainComponent>
     </div>
 }
