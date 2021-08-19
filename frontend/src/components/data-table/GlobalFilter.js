1b6143f06ecc51334ccc80888d1ec678bc871c8c
eminokic <emin.okic@mavenwave.com>
2021-08-19 09:54:00 -0500

    doc

--- frontend/src/components/data-table/GlobalFilter.js
+++ frontend/src/components/data-table/GlobalFilter.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+/**
+ * Global Filter Constant 
+ * 
+ * @param {*} param0 
+ * @returns Global Filter
+ */
 export const GlobalFilter = ({filter, setFilter}) => {
 
     return (
