4738509096701e3439c287fac170166a22a8d31e
Amar Patel <amar.patel@mavenwave.com>
2021-08-19 09:53:17 -0500

    comments

--- frontend/src/components/charts/bargraph.js
+++ frontend/src/components/charts/bargraph.js
@@ -1,3 +1,4 @@
+//This renders the bargraph in the data section
 import React from "react";
 import '../../css/App.css';
 import { api } from '../../api';
@@ -14,7 +15,7 @@ import {
   Bar,
 } from "recharts";
 
-export default function BarGraph(){
+export default function BarGraph(){ //normdata contains the average values of all In-N-Out
   const normdata = [{"ID": "Average Values", 'Land Value (million)': 1.771756678, 'Square Footage (thousand)':3.034135593, 'Elevation (hundred)':4.299610169, 
   'Shore Distance (miles)':13.46314146, '100y Flood Distance (miles)': 3.176161176, '500y Flood Distance (miles)':3.01448598894593},
                     ];
@@ -22,10 +23,6 @@ export default function BarGraph(){
     <BarGraphMain data={normdata}></BarGraphMain>
   </div>
 }
-// var mock_data = all_data.find(x => x.ID === 30)
-// console.log("This is the mock_data", mock_data)
-  // console.log('This is normdata',[normdata[0]])
-  // const data= normdata.data;
 const BarGraphMain = (props) => {
   const data = props.data
     return <div>
@@ -43,14 +40,12 @@ const BarGraphMain = (props) => {
         >
           <XAxis
             dataKey="ID"
-            // scale="point"
-            // padding={{ left: 10, right: 10 }}
           />
           <YAxis />
           <Tooltip />
           <Legend />
           <CartesianGrid strokeDasharray="3 3" />
-          <Bar dataKey="Land Value (million)" fill="#262262" />
+          <Bar dataKey="Land Value (million)" fill="#262262" /> 
           <Bar dataKey="Square Footage (thousand)" fill="#623074" />
           <Bar dataKey="Elevation (hundred)" fill="#9b1d94"/>
           <Bar dataKey="Shore Distance (miles)" fill="#e50695"/>
