ff930a8beee7d9b35c0d8ec8ffd62d8167561bd2
eminokic <emin.okic@mavenwave.com>
2021-08-19 10:57:40 -0500

    Splash!!!

--- frontend/src/main/App.js
+++ frontend/src/main/App.js
@@ -8,6 +8,7 @@ import '../css/App.css';
 
 import MainComponent from "../components/main-page/main-component";
 
+import SplashScreenComponent from "../components/splash-screen/splash-component.js"
 export default function App() {
 
   /**
@@ -34,6 +35,8 @@ export default function App() {
   if(!isLoaded) {return "Rendering Map...";}
 
   return <div>
+    <SplashScreenComponent>
     <MainComponent></MainComponent>
+    </SplashScreenComponent>
     </div>
 }
